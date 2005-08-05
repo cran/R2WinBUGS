9ce89306a233e4698a00717e9e6a5dae81207f5a
Sibylle Sturtz <sturtz@statistik.uni-dortmund.de>
2005-08-05 00:00:00 +0000

    version 1.0-0

--- R/attach.all.R
+++ R/attach.all.R
@@ -1,14 +1,32 @@
-attach.all <- function(.a, overwrite = FALSE){
-  if(class(.a) != "bugs")
-    stop("attach.all() requires a bugs object.")
-  .a <- c(.a, .a$sims.list)
-  if (overwrite){
-    for (j in 1:length(.a)){
-      if (names(.a)[j] %in% ls(.GlobalEnv))
-        remove (list=names(.a)[j], envir=.GlobalEnv)
-    }
-  }
-  attach(.a)
+attach.all <- function(x, overwrite = NA, name = "attach.all"){
+    rem <- names(x) %in% ls(.GlobalEnv)
+    if(!any(rem)) overwrite <- FALSE
+    rem <- names(x)[rem]
+    if(is.na(overwrite))
+        if(interactive()){
+            overwrite <- "YES" == winDialog(type = "yesno", 
+                paste("The following objects in .GlobalEnv will mask\nobjects in the attached database:\n", 
+                      paste(rem, collapse=", "), 
+                      "\nRemove these objects from .GlobalEnv?", sep=""))
+        }
+        else overwrite <- FALSE
+    if(overwrite) remove(list=rem, envir=.GlobalEnv)
+    attach(x, name=name)
 }
 
-detach.all <- function() detach(.a)
+attach.bugs <- function (x, overwrite = NA){
+  if(class(x) != "bugs")
+    stop("attach.all() requires a bugs object.")
+  r1 <- attach.all(x, overwrite = overwrite, name = "bugs.all")
+  r2 <- attach.all(x$sims.list, overwrite = overwrite, name = "bugs.sims")
+  invisible(c(bugs.all=r1, bugs.sims=r2))
+}
+
+
+detach.all <- function(name = "attach.all")
+    do.call("detach", list(name=name))
+
+detach.bugs <- function(){
+    detach.all("bugs.all")
+    detach.all("bugs.sims")
+}
