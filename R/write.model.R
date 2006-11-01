3a64b1f160d48d18a523d82190c68b0f24b3f41a
Sibylle Sturtz <sturtz@statistik.uni-dortmund.de>
2006-11-01 00:00:00 +0000

    version 2.0-4

--- R/write.model.R
+++ R/write.model.R
@@ -2,6 +2,6 @@ write.model <- function(model, con = "model.bug")
 {
   model.text <- attr(model, "source")
   model.text <- sub("^\\s*function\\s*\\(\\s*\\)", "model", model.text)
-  model.text <- sub("%_%", "", model.text)
+  model.text <- gsub("%_%", "", model.text)
   writeLines(model.text, con = con)
 }
